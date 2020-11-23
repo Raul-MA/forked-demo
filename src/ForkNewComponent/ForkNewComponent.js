2e6fa6de60ce72c0b9f95f1e09c023ea68eb3714
Raul-MA <coeus@coeuss-Mac-mini.local>
2020-11-23 09:48:23 -0600

    Change text

---
+++ src/ForkNewComponent/ForkNewComponent.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const ForkNewComponent = props => {
+  return (
+    <div>
+      Is my new component in fork
+    </div>
+  )
+}
+
+ForkNewComponent.propTypes = {
+
+}
+
+export default ForkNewComponent
